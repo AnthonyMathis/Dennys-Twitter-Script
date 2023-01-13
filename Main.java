ee8a20c517fb6fa8d4e608df727ecaad0d80811a
AnthonyMathis <113065654+AnthonyMathis@users.noreply.github.com>
2023-01-13 04:15:30 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ public class Main{
 
     public static void main(String []args) throws TwitterException, InterruptedException{
         String user="DennysDiner";
-        String message="First";
+        String message="";
         String consumerKey="";
         String consumerSecret="";
         String accessToken="";
